b3d1be059f09630dc377427a2ec76d637eb10b67
Celiacaoyanan <celiacaoyanan@outlook.com>
2016-10-15 17:25:01 +0800

    Change to English annotation

--- process_corpus.py
+++ process_corpus.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import sys
+"""
+Process the raw corpus
+Example of one line in raw corpus: 12	ruby	Ruby is a multi-platform open-source, dynamic object-oriented interpreted language, created by Yukihiro Matsumoto (Matz) in 1995.
+Example of one line in corpus after processed: Ruby is a multi-platform open-source dynamic object-oriented interpreted language created by Yukihiro Matsumoto Matz in 1995
+"""
 
 if __name__ == '__main__':
 
     def process_corpus(corpus_dir):
-        f = open('corpus.txt', 'w')
-        f1 = open('excerpts.tsv', 'r')
-        f2 = f1.readlines()
-        for line in f2:
+        f_processed_corpus = open('corpus.txt', 'w')  # corpus.txt is used to store the corpus that are processed
+        f_raw_corpus = open(corpus_dir, 'r')
+        f_oneline_raw_corpus = f_raw_corpus.readlines()
+        for line in f_oneline_raw_corpus:
             for i in (',', '(', ')', '/', '.', ' ', ' ', ':', '[', ']', '`', '"', '"'):
-                line = line.replace(i, ' ')
-            list = line.split()
-            for w in list[2:]:
+                line = line.replace(i, ' ')  # replace all the punctuation mark with a space
+            list = line.split()  # put every word of a line into a list
+            for w in list[2:]:  # leave out the first two words(the line number and the key word)
                 w = w.strip()
-                f.write(w)
-                f.write(" ")
-            f.write('\n')
+                f_processed_corpus.write(w)
+                f_processed_corpus.write(" ")
+            f_processed_corpus.write('\n')
 
     import optparse
 
